c8e0ea2972d899203718ccf21470a6fa5597959a
Carolyn Santana <carolyn.ardjo@gmail.com>
2026-05-01 22:21:37 +0700

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Carolyn Santana on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camera
+//
+//  Created by Carolyn Santana on 01/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
